b4e7df4b77a436aa12925eea3890f403b0ad9e57
AbhinandanSengar <abhinandansengar@gmail.com>
2025-05-13 01:57:09 +0530

    Added database(db.js)  file

---
+++ db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, unique: true, required: true }
+});
+
+const adminSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, unique: true, required: true }
+});
+
+const courseSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    imageUrl: { type: String, required: true },
+    creatorId: { type: ObjectId }
+});
+
+const purchaseSchema = new Schema({
+    userId: { type: ObjectId },
+    courseId: { type: ObjectId }
+});
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
