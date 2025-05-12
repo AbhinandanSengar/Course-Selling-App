7b557f0b6466d5002cbadfe364378febf3ccd937
AbhinandanSengar <abhinandansengar@gmail.com>
2025-05-13 01:56:41 +0530

    Added config file

---
+++ config.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+
+
+module.exports = {
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+}
